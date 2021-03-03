ff299026454e40c19e8c0a8818ba634b6e6893b5
Daniel Lamando <dan@danopia.net>
2021-03-03 08:05:49 +0100

    Bump kubernetes client library

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,4 @@ RUN ["deno", "cache", "deps.ts"]
 ADD src/ .
 RUN ["deno", "cache", "mod.ts"]
 
-ENTRYPOINT ["deno", "run", "--unstable", "--allow-net", "--allow-hrtime", "--allow-read=/var/run/secrets/kubernetes.io", "--allow-env", "--cert=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt", "--cached-only", "mod.ts"]
+ENTRYPOINT ["deno", "run", "--unstable", "--allow-net", "--allow-hrtime", "--allow-read=/var/run/secrets/kubernetes.io", "--allow-env", "--cached-only", "mod.ts"]
--- src/deps.ts
+++ src/deps.ts
@@ -13,19 +13,17 @@ export {
 
 export type {
   RestClient as KubernetesClient,
-} from "https://deno.land/x/kubernetes_client@v0.1.3/mod.ts";
+} from "https://deno.land/x/kubernetes_client@v0.2.0/mod.ts";
 export {
   // RestClient as KubernetesClient,
   autoDetectClient as autoDetectKubernetesClient,
   ReadLineTransformer,
   Reflector,
-} from "https://deno.land/x/kubernetes_client@v0.1.3/mod.ts";
+} from "https://deno.land/x/kubernetes_client@v0.2.0/mod.ts";
 
-// TODO using patch to fix proxy functions
-// export * as CoreV1 from "https://deno.land/x/kubernetes_apis@v0.2.0/builtin/core@v1/mod.ts";
-export * as CoreV1 from "https://raw.githubusercontent.com/cloudydeno/deno-kubernetes_apis/main/lib/builtin/core@v1/mod.ts";
-export * as AppsV1 from "https://deno.land/x/kubernetes_apis@v0.2.0/builtin/apps@v1/mod.ts";
-export * as MetaV1 from "https://deno.land/x/kubernetes_apis@v0.2.0/builtin/meta@v1/structs.ts";
+export * as CoreV1 from "https://deno.land/x/kubernetes_apis@v0.3.0/builtin/core@v1/mod.ts";
+export * as AppsV1 from "https://deno.land/x/kubernetes_apis@v0.3.0/builtin/apps@v1/mod.ts";
+export * as MetaV1 from "https://deno.land/x/kubernetes_apis@v0.3.0/builtin/meta@v1/structs.ts";
 
 //------------
 
