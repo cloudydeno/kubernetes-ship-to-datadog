d03fe1ff3e5593d485b2ed3a81c3550ea737dc94
Daniel Lamando <dan@danopia.net>
2021-05-02 10:34:10 +0200

    deps: Use 'published' OpenMetrics publisher

--- src/deps.ts
+++ src/deps.ts
@@ -2,14 +2,11 @@ export { default as DatadogApi } from "https://deno.land/x/datadog_api@v0.1.3/mo
 export { CheckStatus } from "https://deno.land/x/datadog_api@v0.1.3/mod.ts";
 export type { MetricSubmission } from "https://deno.land/x/datadog_api@v0.1.3/v1/metrics.ts";
 
+// from https://github.com/cloudydeno/deno-bitesized :
 export { fixedInterval } from "https://crux.land/4MC9JG#fixed-interval@v1";
 
-export {
-  runMetricsServer,
-} from "https://raw.githubusercontent.com/cloudydeno/deno-openmetrics_exporter/49ce410657ae5cbd9f647acf1233656933a936aa/mod.ts";
-export {
-  replaceGlobalFetch,
-} from "https://raw.githubusercontent.com/cloudydeno/deno-openmetrics_exporter/49ce410657ae5cbd9f647acf1233656933a936aa/lib/instrumented/fetch.ts";
+export { runMetricsServer } from "https://deno.land/x/observability@v0.1.0/sinks/openmetrics/server.ts";
+export { replaceGlobalFetch } from "https://deno.land/x/observability@v0.1.0/sources/fetch.ts";
 
 export type {
   RestClient as KubernetesClient,
@@ -29,10 +26,7 @@ export * as MetaV1 from "https://deno.land/x/kubernetes_apis@v0.3.0/builtin/meta
 
 import { bufferWithCount } from "https://deno.land/x/stream_observables@v1.0/transforms/buffer-with-count.ts";
 import { filter } from "https://deno.land/x/stream_observables@v1.0/transforms/filter.ts";
-
-import {
-  fromIterable,
-} from "https://deno.land/x/stream_observables@v1.0/sources/from-iterable.ts";
+import { fromIterable } from "https://deno.land/x/stream_observables@v1.0/sources/from-iterable.ts";
 
 export const ows = {
   bufferWithCount,
