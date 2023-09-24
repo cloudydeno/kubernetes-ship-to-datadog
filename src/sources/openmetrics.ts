a4c9ce79c85136d566d747cd9d1b82b7ba73e603
Daniel Lamando <dan@danopia.net>
2023-09-24 16:46:49 +0200

    Extract out OpenMetrics parsing code for reuse

--- src/sources/openmetrics.ts
+++ src/sources/openmetrics.ts
@@ -1,120 +1,18 @@
 import {
   autoDetectKubernetesClient,
   CoreV1,
-  MetricSubmission,
-  ReadLineTransformer,
 } from '../deps.ts';
 import { KubeWatcher } from "../lib/kube-watcher.ts";
 import {
   AsyncMetricGen,
   makeLoopErrorPoint,
 } from '../lib/metrics.ts';
-
-interface RawMetric {
-  name: string;
-  help?: string;
-  unit?: string;
-  type: string;
-  datas: Array<MetricPoint>;
-}
-interface MetricPoint {
-  submetric: string;
-  labelset: string;
-  tags: [string, string][];
-  facets: Record<string,string>;
-  value: number;
-  rawValue: string;
-}
+import { OpenmetricsMemory, parseMetrics } from "../lib/openmetrics.ts";
 
 const kubernetes = await autoDetectKubernetesClient();
 const coreApi = new CoreV1.CoreV1Api(kubernetes);
 
-const monotonicMemory = new Map<string,number>();
-
-async function* parseMetrics(stream: ReadableStream<Uint8Array>): AsyncGenerator<RawMetric> {
-  const lines = stream.pipeThrough(new ReadLineTransformer());
-  let currName: string | undefined;
-  let currHelp: string | undefined;
-  let currType: string | undefined;
-  let currUnit: string | undefined;
-  let datas: Array<MetricPoint> | undefined;
-  for await (const line of lines) {
-    if (!line.startsWith('#')) {
-      // console.log(3, currName, currHelp, currType, line);
-      const match = line.match(/^([^ {]+)(?:{([^}]+)}|) ([^ ]+)$/);
-      if (!match) throw new Error(`TODO: ${line}`);
-      const tags: Record<string,string> = Object.create(null);
-      for (const kv of match[2]?.split(',') ?? []) {
-        const [k,v] = kv.split('=');
-        tags[k] = JSON.parse(v);
-      }
-      datas!.push({
-        submetric: match[1].slice(currName!.length + 1),
-        labelset: match[2],
-        tags: match[2]?.split(',').map(str => {
-          const [k, v] = str.split('=');
-          return [k, JSON.parse(v)];
-        }) ?? [],
-        facets: tags,
-        value: parseFloat(match[3]),
-        rawValue: match[3],
-      });
-    } else if (line.startsWith('# TYPE ')) {
-      if (datas) {
-        yield {name: currName!, help: currHelp, unit: currUnit, type: currType!, datas};
-      }
-      currName = line.split(' ')[2];
-      currHelp = undefined;
-      currUnit = undefined;
-      currType = line.slice(8+currName.length);
-      datas = [];
-    } else if (line.startsWith('# HELP ')) {
-      currName = line.split(' ')[2];
-      currHelp = line.slice(8+currName.length);
-    } else if (line.startsWith('# UNIT ')) {
-      currName = line.split(' ')[2];
-      currUnit = line.slice(8+currName.length);
-    } else if (line === '# EOF') {
-      // break;
-    } else throw new Error("TODO: "+line);
-  }
-  if (datas) {
-    yield {name: currName!, help: currHelp, unit: currUnit, type: currType!, datas};
-  }
-}
-
-function reportPointAs(
-  point: MetricPoint,
-  metric_name: string,
-  metric_type: 'gauge' | 'rate' | 'count',
-  extraTags: string[],
-  tagKeyMap: Record<string,string>,
-  monotonicKey: string | false,
-): MetricSubmission[] {
-
-  let value = point.value;
-  if (monotonicKey) {
-    const lastSeen = monotonicMemory.get(monotonicKey);
-    monotonicMemory.set(monotonicKey, value);
-    // console.log(monotonicKey, value, lastSeen);
-    if (typeof lastSeen === 'number') {
-      value -= lastSeen;
-      if (value < 0) return [];
-    } else {
-      return [];
-    }
-  }
-
-  return [{
-    metric_name,
-    points: [{value: value}],
-    interval: 30,
-    metric_type,
-    tags: [
-      ...extraTags,
-      ...point.tags.map(([k,v]) => (tagKeyMap[k]||`om_${k}`)+`:${v}`),
-    ]}];
-}
+const memory = new OpenmetricsMemory();
 
 export async function* buildOpenMetrics(baseTags: string[], watcher: KubeWatcher): AsyncMetricGen {
 
@@ -174,27 +72,27 @@ export async function* buildOpenMetrics(baseTags: string[], watcher: KubeWatcher
           case 'counter':
             for (const point of rawMetric.datas) {
               if (point.submetric && point.submetric !== 'total') continue;
-              yield* reportPointAs(point, metric_name, 'count', podTags, {}, `${memPrefix}!${point.submetric}!${point.labelset}`);
+              yield* memory.reportPointAs(point, metric_name, 'count', podTags, {}, `${memPrefix}!${point.submetric}!${point.labelset}`);
             }
             break;
 
           case 'gauge':
             for (const point of rawMetric.datas) {
               if (point.submetric) continue;
-              yield* reportPointAs(point, metric_name, 'gauge', podTags, {}, false);
+              yield* memory.reportPointAs(point, metric_name, 'gauge', podTags, {}, false);
             }
             break;
 
           case 'summary':
             for (const point of rawMetric.datas) {
               if (point.submetric !== 'sum' && point.submetric !== 'count') continue;
-              yield* reportPointAs(point, metric_name+'.'+point.submetric, 'count', podTags, {}, `${memPrefix}!${point.submetric}!${point.labelset}`);
+              yield* memory.reportPointAs(point, metric_name+'.'+point.submetric, 'count', podTags, {}, `${memPrefix}!${point.submetric}!${point.labelset}`);
             }
             break;
 
           case 'histogram':
             for (const point of rawMetric.datas) {
-              yield* reportPointAs(point, metric_name+'.'+point.submetric, 'count', podTags, {}, `${memPrefix}!${point.submetric}!${point.labelset}`);
+              yield* memory.reportPointAs(point, metric_name+'.'+point.submetric, 'count', podTags, {}, `${memPrefix}!${point.submetric}!${point.labelset}`);
             }
             break;
 
